534bd436511dad5c12be05a782619c2c423bbec0
tung1100i <1851063190@e.tlu.edu.vn>
2022-04-29 17:00:58 +0700

    Rout - app/Controller

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'));
 
 
 // Template engine
@@ -19,15 +25,9 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 
-
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/news', (req, res) => {
-    res.render('news')
-})
+// Route init
+route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port at http://localhost:${port}`)
-})
+    console.log(`Example app listening on port at http://localhost:${port}`);
+});
